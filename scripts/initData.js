d063b2562f72d3700d4e67ec415c09cd4fc2ad8c
jdlrobson <jdlrobson@gmail.com>
2019-10-14 15:23:05 -0700

    Drop countries from sights and go next

--- scripts/initData.js
+++ scripts/initData.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 import destinations from './data/destinations.json';
 import next from './data/next.json';
-import { extractCard, getMissingKeys } from './utils';
+import countries from './data/countries.json';
 import fs from 'fs';
 
 const pending = [];
@@ -16,6 +16,17 @@ Object.keys(next).forEach((key) => {
     }
 });
 
+console.log('Checking countries');
+Object.keys(countries).forEach((countryName) => {
+    const country = countries[countryName];
+    const newSights = country.sights.filter((sight) => !countries[sight.title]);
+    if ( newSights.length !== country.sights.length ) {
+        console.log(`Country ${countryName} listed another country as a sight.`);
+        countries[countryName].sights = newSights;
+        pending.push(Promise.resolve())
+    }
+})
+
 console.log('Checking go next is 2-way...');
 Object.keys(destinations).forEach(( destinationTitle ) => {
     const place = destinations[destinationTitle];
@@ -27,9 +38,12 @@ Object.keys(destinations).forEach(( destinationTitle ) => {
             pending.push(Promise.resolve())
         }
     })
-    const newSights = place.sights.filter((sight) => !(next[place.title] || []).includes(sight.title))
+    const newSights = place.sights.filter((sight) =>
+        !(next[place.title] || []).includes(sight.title) &&
+        (!countries[sight.title])
+    );
     if ( newSights.length !== place.sights.length) {
-        console.log(`Removed sight that is go next in ${place.title}`);
+        console.log(`Removed sight that is go next/country in ${place.title}`);
         destinations[destinationTitle].sights = newSights;
         pending.push(Promise.resolve())
     }
@@ -42,6 +56,7 @@ Object.keys(destinations).forEach(( destinationTitle ) => {
 if ( pending.length ) {
     console.log('Updating JSON');
     Promise.all( pending ).then(() => {
+        fs.writeFileSync(`${__dirname}/data/countries.json`, JSON.stringify(countries));
         fs.writeFileSync(`${__dirname}/data/destinations.json`, JSON.stringify(destinations));
         fs.writeFileSync(`${__dirname}/data/next.json`, JSON.stringify(next));
     });
