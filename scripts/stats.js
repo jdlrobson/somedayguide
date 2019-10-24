171ee9fa26ad7ffa933f24162621aba3c8d0b9b8
jdlrobson <jdlrobson@gmail.com>
2019-10-23 22:23:57 -0700

    Stats: Places without go next, unpopular sights
    No place should not link to another place to go next to
    Uncover those in the stats
    
    Flag sights which have not been used much

--- scripts/stats.js
+++ scripts/stats.js
@@ -3,26 +3,36 @@ import sights_json from './data/sights.json';
 import countries_json from './data/countries.json';
 import next from './data/next.json';
 const MIN_SIGHTS = 2;
+const MIN_GO_NEXT = 2;
 const MIN_SIGHTS_COUNTRY = 2;
+const MIN_SIGHT_USAGES_TO_BE_POPULAR = 2;
 const MIN_DESTINATIONS_COUNTRY = 4;
 
-const usedsights = new Set();
+const sightCount = {};
 
 const countries = Object.keys(countries_json)
 Object.keys(countries_json).map((country) => countries_json[country].sights).forEach((sights) => {
     sights.forEach((sight)=> {
-        usedsights.add(sight);
+        if ( !sightCount[sight] ) {
+            sightCount[sight] = 1;
+        } else {
+            sightCount[sight] = sightCount[sight] + 1;
+        }
     });
 });
 
 Object.keys(destinations_json).map((place) => destinations_json[place].sights).forEach((sights) => {
     sights.forEach((sight)=> {
-        usedsights.add(sight);
+        if ( !sightCount[sight] ) {
+            sightCount[sight] = 1;
+        } else {
+            sightCount[sight] = sightCount[sight] + 1;
+        }
     });
 });
 
 const unusedsights = Object.keys(sights_json).filter((sight) => {
-    return !usedsights.has(sight);
+    return !sightCount[sight];
 });
 const sightsnothumb = Object.keys(sights_json).filter((sight) => {
     return !sights_json[sight].thumbnail;
@@ -36,7 +46,7 @@ const destinationsnothumb = destinations.filter((t) => {
 });
 const no_climate = destinations.filter((key) => !destinations_json[key].climate || destinations_json[key].climate.length === 0);
 const lacking_sights = destinations.filter((key) => (destinations_json[key].sights || []).length < MIN_SIGHTS);
-const lacking_gonext = destinations.filter((key) => !next[key] || next[key].length < 1);
+const lacking_gonext = destinations.filter((key) => !next[key] || next[key].length < MIN_GO_NEXT);
 const nosightsnonext = lacking_gonext.filter((title) =>
     no_climate.indexOf(title) > -1 && lacking_sights.indexOf(title) > -1);
 const countrylackingsights = countries.filter((title) => {
@@ -45,6 +55,7 @@ const countrylackingsights = countries.filter((title) => {
 const countrylackingdestinations = countries.filter((title) => {
     return (countries_json[title].destinations || []).length < MIN_DESTINATIONS_COUNTRY;
 });
+const unpopularsights = Object.keys(sightCount).filter((sight) => sightCount[sight] < MIN_SIGHT_USAGES_TO_BE_POPULAR);
 console.log(`Do not have a climate widget: ${no_climate.length}/${destinations.length}`);
 console.log(`Total sights: ${Object.keys(sights_json).length}`);
 console.log(`Destinations lacking sights: ${lacking_sights.length}/${destinations.length}`);
@@ -53,12 +64,13 @@ console.log(`Countries lacking destinations: ${countrylackingdestinations.length
 console.log(`Lacking next: ${lacking_gonext.length}/${destinations.length}`);
 console.log(`Lacking all: ${nosightsnonext.length}`);
 console.log(`Unused sights: ${unusedsights.length}`);
+console.log(`Unpopular sights: ${unpopularsights.length}`);
 console.log(`Sights without thumbnail: ${sightsnothumb.length}`);
 console.log(`Sights without lon/lat: ${sightsnolonglat.length}`);
 console.log(`Destinations without thumbnail: ${destinationsnothumb.length}`);
 
-console.log(countrylackingdestinations);
 export {
+    lacking_gonext,
     countrylackingdestinations,
     countrylackingsights,
     unusedsights,
