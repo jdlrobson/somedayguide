168e0aa4f136a04c0cc78ca1ccd8a5a69c1d68ef
jdlrobson <jdlrobson@gmail.com>
2019-10-25 22:06:34 -0700

    link linz

--- ui/components/Footer/index.jsx
+++ ui/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import preact, { h } from 'preact';
 export default function ( props ) {
     const { title } = props;
     return <footer class={props.class}>
-        Someday guide is a <a href="https://jdlrobson.com">Jon Robson</a> and Linz Lim production with data mashed together
+        Someday guide is a <a href="https://jdlrobson.com">Jon Robson</a> and <a href="linzybearswings.wordpress.com">Linz Lim</a> production with data mashed together
         from <a href='https://en.wikipedia.org/'>Wikipedia</a> and <a href={`https://en.wikivoyage.org/wiki/${title}`}>Wikivoyage</a>.<br/>
         An open web is a happy web. If anything looks wrong click
         the <a href={`https://en.wikivoyage.org/wiki/${title}?action=edit`}>edit button</a> and/or 
