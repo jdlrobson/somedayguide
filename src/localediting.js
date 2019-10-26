1a6dd566067f8bfeef6d4d264dac4e036a9ca738
jdlrobson <jdlrobson@gmail.com>
2019-10-26 15:14:00 -0700

    opt in admin mode

--- src/localediting.js
+++ src/localediting.js
@@ -1,5 +1,18 @@
 const LOCAL_NOTE = window.location.pathname;
 
+function enableAdminMode() {
+    const path = window.location.pathname.split('/');
+    const title = path[path.length-1];
+    const a = document.createElement('a');
+    a.setAttribute('href', `https://github.com/jdlrobson/somedayguide/wiki/${title}`);
+    a.textContent = 'Edit';
+    document.querySelectorAll('.note')[0].appendChild(a);
+}
+
+let lastKeys = [];
+if (localStorage.getItem('admin--flag')) {
+    enableAdminMode();
+}
 export default function setupLocalEditing() {
     const localNote = document.querySelector('#local-edit [contenteditable]'),
         note = localStorage.getItem(LOCAL_NOTE);
@@ -8,5 +21,13 @@ export default function setupLocalEditing() {
     }
     localNote.addEventListener('input', function (ev) {
         localStorage.setItem(LOCAL_NOTE, this.textContent);
+        lastKeys.push(ev.data);
+        if (lastKeys.length > 9) {
+            if (lastKeys.join('').indexOf('adminbear') > -1) {
+                localStorage.setItem('admin--flag', 1);
+                enableAdminMode();
+            }
+            lastKeys = lastKeys.slice(1);
+        }
     })
 }
