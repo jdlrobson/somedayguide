382773b5b0d317569929cfdd61784c6887b83400
jdlrobson <jdlrobson@gmail.com>
2019-11-02 22:15:05 -0700

    Well formed thumbnail redirects generated to public folder

--- scripts/generateIndex.js
+++ scripts/generateIndex.js
@@ -22,16 +22,23 @@ console.log('Updating JSON');
 fs.writeFileSync(`${__dirname}/../public/index.json`, JSON.stringify(index));
 fs.writeFileSync(`${__dirname}/../public/index--sights.json`, JSON.stringify(indexSights));
 
+const getThumb = (thumbnail) => {
+    if (thumbnail && thumbnail.indexOf('//') === 0) {
+        return 'https:' + thumbnail;
+    } else {
+        return thumbnail;
+    }
+}
 const thumbs = Object.keys(countries).map(
-        (key) => [`/thumbnails/country/${key}`, countries[key].thumbnail ]
+        (key) => [`/thumbnails/country/${key}`, getThumb(countries[key].thumbnail)]
     )
     .concat(
         Object.keys(destinations).map(
-            (key) => [`/thumbnails/destination/${key}`, `https:${destinations[key].thumbnail}` ]
+            (key) => [`/thumbnails/destination/${key}`, getThumb(destinations[key].thumbnail)]
         )
     ).filter((redirectrule) => redirectrule[1] !== undefined);
 
 // redirects (in future why don't we download these?)
-fs.writeFileSync(`${__dirname}/../_redirects`,
+fs.writeFileSync(`${__dirname}/../public/_redirects`,
     thumbs.map((redirectrule) => redirectrule.join(' ')).join('\n')
 );
