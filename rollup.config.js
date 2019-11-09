082bbcf4ed8b28288ac2d814524b27a0f16a920c
jdlrobson <jdlrobson@gmail.com>
2019-11-09 10:16:18 -0800

    FIx dev environment - watching and terser disabled in dev mode

--- rollup.config.js
+++ rollup.config.js
@@ -2,23 +2,29 @@ import { terser } from 'rollup-plugin-terser';
 import resolve from 'rollup-plugin-node-resolve';
 import postcss from 'rollup-plugin-postcss'
 import commonjs from 'rollup-plugin-commonjs';
+const MODE = process.env.MODE;
 
 const pluginsjsonly = [
     resolve(),
     commonjs(),
-    terser(),
     postcss()
 ];
+
+const pluginsindex = [
+    resolve(),
+    commonjs(),
+    postcss({extract: true, minimize: true})
+];
+
+if (MODE === 'production') {
+    pluginsjsonly.push(terser());
+    pluginsindex.push(terser());
+}
 
 export default [
     {
         input: 'src/index.js',
-        plugins: [
-            resolve(),
-            commonjs(),
-            terser(),
-            postcss({extract: true, minimize: true})
-        ],
+        plugins: pluginsindex,
         output: {
             format: 'iife',
             dir: 'public/'
