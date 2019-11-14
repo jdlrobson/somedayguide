06d2a5a66aba88e4063ad31c6747f9fa85943fd8
jdlrobson <jdlrobson@gmail.com>
2019-11-13 21:38:34 -0800

    Restrict height of instagram embed

--- ui/components/InstagramEmbed/index.jsx
+++ ui/components/InstagramEmbed/index.jsx
@@ -6,7 +6,7 @@ export default function ( { id } ) {
             data-instgrm-captioned
             data-instgrm-permalink={`https://www.instagram.com/p/${id}/?utm_source=ig_embed&amp;utm_campaign=loading`}
             data-instgrm-version="12"
-            style="max-width: 470px; box-sizing: border-box; height: 900px;background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"
+            style="max-width: 470px; box-sizing: border-box; height: 690px;background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"
             ><div style="padding:16px;">
                 <a href={`https://www.instagram.com/p/${id}/?utm_source=ig_embed&amp;utm_campaign=loading`}>View on Instagram</a>
             </div>
