a3dd13ca55175f0a40ed1b067a061270097f24a8
jdlrobson <jdlrobson@gmail.com>
2019-12-21 10:54:50 -0800

    Private nodes need base class

--- ui/components/Note.jsx
+++ ui/components/Note.jsx
@@ -1,6 +1,6 @@
 import preact, { h } from 'preact';
 
 export default function ( { children, isprivate, id }) {
-    const className = isprivate ? 'somedaynotenote--private' : 'somedaynote';
+    const className = isprivate ? 'somedaynote somedaynotenote--private' : 'somedaynote';
     return <div id={id} class={className}>{children}</div>
 }
