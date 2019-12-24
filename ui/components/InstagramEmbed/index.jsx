c7e027e85919b5b3d826fa6e473da165423e3c09
jdlrobson <jdlrobson@gmail.com>
2019-12-24 09:37:07 -0800

    Instagrams are expensive
    Just use an img tag.

--- ui/components/InstagramEmbed/index.jsx
+++ ui/components/InstagramEmbed/index.jsx
@@ -2,14 +2,9 @@ import preact, { h } from 'preact';
 
 export default function ( { id, className } ) {
     return <div class={className}>
-            <blockquote class="instagram-media"
-            data-instgrm-captioned
-            data-instgrm-permalink={`https://www.instagram.com/p/${id}/?utm_source=ig_embed&amp;utm_campaign=loading`}
-            data-instgrm-version="12"
-            style="max-width: 470px; box-sizing: border-box; height: 690px;background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"
-            ><div style="padding:16px;">
-                <a href={`https://www.instagram.com/p/${id}/?utm_source=ig_embed&amp;utm_campaign=loading`}>View on Instagram</a>
-            </div>
-        </blockquote>
+            <img src={`https://instagram.com/p/${id}/media/?size=l`}
+                alt="Jon and Linzy Instagram photo" />
+            <a class="slideshow__slide__caption"
+                href={`https://instagram.com/p/${id}`}>View on Instagram</a>
     </div>;
 }
